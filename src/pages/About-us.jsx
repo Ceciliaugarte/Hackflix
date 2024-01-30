72083d97fa7c78f270f046c981f17d7da2df9259
Ceciliaugarte <cecilia.ugartemendia@student.ha.dev>
2024-01-30 13:13:23 -0300

    mx-2

--- src/pages/About-us.jsx
+++ src/pages/About-us.jsx
@@ -7,39 +7,33 @@ function AboutUs() {
 
   return (
     <div>
-      {/* <header
-        className="headerImg"
-        style={{
-          backgroundImage: `url("https://naanmedia.in/wp-content/uploads/2022/10/3658600.jpg")`,
-        }}
-      ></header> */}
       <div className="container my-5 pt-5 about-us">
         <div className="row justify-content-center mb-2">
           <div className="col-12 col-lg-10">
             <h1 className="text-center mb-4 general-text fw-bold ">
               ABOUT US:
             </h1>
-            <p className="general-text fs-5">
+            <p className="general-text fs-5 mx-2">
               Step into the world of Hackflix, where movie magic meets the
               convenience of online streaming. As a premier platform, Hackflix
               offers a curated selection of films, ensuring a captivating
               cinematic adventure right at your fingertips.
             </p>
-            <p className="general-text fs-5">
+            <p className="general-text fs-5 mx-2">
               Navigating through our user-friendly interface, you'll discover a
               treasure trove of movies spanning various genres. From Hollywood
               blockbusters that keep you on the edge of your seat to independent
               gems waiting to be uncovered, Hackflix caters to the diverse
               tastes of every movie enthusiast.
             </p>
-            <p className="general-text fs-5">
+            <p className="general-text fs-5 mx-2">
               Our goal is to provide you with a seamless streaming experience,
               bringing the latest releases, timeless classics, and hidden gems
               directly to your screen. Whether you're in the mood for thrilling
               tales or heartwarming narratives, Hackflix is your go-to
               destination for cinematic exploration
             </p>
-            <p className="general-text fs-5">
+            <p className="general-text fs-5 mx-2">
               With Hackflix, the joy of discovering and enjoying exceptional
               films is just a click away. Elevate your movie nights and immerse
               yourself in a world of entertainment crafted just for you.
